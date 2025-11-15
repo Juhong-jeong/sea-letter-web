a8441e41e4be05359493ba9682b6c4d21b1550f7
Juhong-jeong <skyofpeople@gmail.com>
2025-11-15 11:16:33 +0900

    초기 세팅

---
+++ src/components/home/hompage.jsx
@@ -0,0 +1,28 @@
+import styled from "@emotion/styled";
+
+const HomePage = () => {
+  return (
+    <Container>
+      test
+      <BackgroundImage src="/background.png" />
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  background-color: #ceb6c0;
+`;
+
+const BackgroundImage = styled.img`
+  width: 100vw;
+  height: 100vh;
+  object-fit: cover;
+  position: absolute;
+  left: 0;
+  top: 0;
+`;
+
+export default HomePage;
